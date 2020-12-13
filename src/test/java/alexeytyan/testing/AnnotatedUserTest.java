3405e1e7cfd48b4541289ff9d05abaa8bf75ed3c
AlexeyTyan <alekseytyan45@gmail.com>
2020-12-13 12:16:10 +0300

    AnnotatedUserTest.java : The same as UserTest.java with Annotations usage

--- src/test/java/alexeytyan/testing/AnnotatedUserTest.java
+++ src/test/java/alexeytyan/testing/AnnotatedUserTest.java
@@ -7,8 +7,6 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
-
 public class AnnotatedUserTest {
     private User user;
     private User user1;
